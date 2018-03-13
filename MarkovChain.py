78178c61655c5a2f22a7cc209225d87963b7801c
Lipry <fabiolipreri@me.com>
2018-03-13 10:06:10 +0100

    First implementation of get_state function in MarkovChain class

--- MarkovChain.py
+++ MarkovChain.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 class MarkovChain:
 
@@ -13,10 +14,14 @@ class MarkovChain:
         if next in values.keys():
             values[next] = values[next]+1
         else:
-            values[next] = 0
+            values[next] = 1
 
         self.states[state] = values
 
+    def get_state(self, t):
+        total = sum(self.states[t].values())
+        prob = [n/total for n in self.states[t].values()]
+        return np.random.choice(list(self.states[t].keys()), 1, p=prob)[0]
 
     #def generate(self, n_words):
     #    last_kgram = random.choice(self.states.keys())
--- main.py
+++ main.py
@@ -8,5 +8,7 @@ if __name__ == "__main__":
     mc.add_state(("world,", "hello"), "Universe")
     mc.add_state(("world,", "hello"), "Pincopallino")
     mc.add_state(("hello", "universe!"), "\n")
-
     print(mc.states)
+
+    for _ in range(0, 5):
+       print(mc.get_state(("world,", "hello")))
